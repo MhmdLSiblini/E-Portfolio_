5bedfcb7966996298e9acc2af50fc62d3e151ffe
MhmdLSiblini <mohammadsiblini246@gmail.com>
2023-03-06 21:53:39 +0200

    7th commit

--- Loader.js
+++ Loader.js
@@ -9,3 +9,25 @@ window.addEventListener('load', function() {
     document.querySelector('.about-me__img').classList.add('fade_in__img');
     document.querySelector('#remove').classList.remove('loading');
 });
+
+// Store the original page title
+var originalTitle = document.title;
+
+// Set a timer variable
+var timer;
+
+// Listen for the visibilitychange event
+document.addEventListener("visibilitychange", function() {
+  // Check if the page is hidden (i.e., user has switched tabs)
+  if (document.hidden) {
+    // Start a timer that updates the page title every 2 seconds
+    timer = setInterval(function() {
+      document.title = (document.title == originalTitle) ? "Future Employee Here! 🖐️" : originalTitle;
+    }, 2000);
+  } else {
+    // Clear the timer and restore the original page title
+    clearInterval(timer);
+    document.title = originalTitle;
+  }
+});
+
